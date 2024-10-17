39e830ecbe84c26b7154b038382485da54c72b82
Dylan Stephenson <dylan.stephenson@studiesweekly.com>
2024-10-16 20:02:21 -0600

    Created mobile view for discover page. Still needs desktop view and Local storage inclusion

---
+++ chamber/scripts/discover.js
@@ -0,0 +1,15 @@
+// Adding updated date and current year
+const today = new Date();
+const currentYear = document.querySelector("#currentyear");
+currentYear.innerHTML = today.getFullYear();
+const lastModified = document.querySelector("#lastModified");
+lastModified.innerHTML = `Last Modified: ${document.lastModified}`;
+
+//creating responsive nav
+const hamburgerElement = document.querySelector("#menuButton");
+const navElement = document.querySelector("#animateMe");
+
+hamburgerElement.addEventListener('click', () => {
+    navElement.classList.toggle('open');
+    hamburgerElement.classList.toggle('open');
+})
